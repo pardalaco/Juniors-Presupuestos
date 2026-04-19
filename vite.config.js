47790f3e1ab4ce6ffb60ddda69a36041a309962b
Daniel Rovira Martínez <pardalaco@gmail.com>
2026-04-19 17:30:27 +0200

    build: deploy bugs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { readdirSync } from "fs";
+
+// Función para obtener todos los archivos .html automáticamente
+const getHtmlInputs = () => {
+  const files = readdirSync(__dirname);
+  const inputs = {};
+
+  files.forEach((file) => {
+    if (file.endsWith(".html")) {
+      // Nombre de la propiedad (sin .html) : Ruta completa
+      const name = file.replace(".html", "");
+      inputs[name] = resolve(__dirname, file);
+    }
+  });
+
+  return inputs;
+};
 
 export default defineConfig({
-  // Es mejor usar el nombre exacto del repo para evitar fallos con las rutas de assets
-  base: "/Juniors-Presupuestos/", 
+  base: "/Juniors-Presupuestos/",
+  build: {
+    rollupOptions: {
+      input: getHtmlInputs(), // <--- Aquí llamamos a la función
+    },
+  },
 });
